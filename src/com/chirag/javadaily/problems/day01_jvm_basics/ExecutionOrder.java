ff093c4abbbcbea088f69088bb4a06784a3f7d9a
PrinceDev <pri1999.m.pm@gmail.com>
2026-01-06 05:26:01 +0530

    Initial clean Java project structure

---
+++ src/com/chirag/javadaily/problems/day01_jvm_basics/ExecutionOrder.java
@@ -0,0 +1,8 @@
+package com.chirag.javadaily.problems.day01_jvm_basics;
+
+public class ExecutionOrder {
+    public static void main(String[] args) {
+        System.out.println("Day 01 – Java Daily Problem Solving started");
+    }
+}
+
